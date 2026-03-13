20fc96e0b4d377e3f38a0bf99d8c9bc4db4bad56
Esabrina77 <146923436+Esabrina77@users.noreply.github.com>
2026-03-13 16:31:36 +0100

    Configure output to 'standalone' for Docker optimization
    Set output to 'standalone' to reduce Docker image size.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Permet de réduire la taille de l'image Docker de 1Go à ~100Mo
 };
 
 export default nextConfig;
